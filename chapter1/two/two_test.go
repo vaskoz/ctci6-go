add7875b543a63252218841c15a45a34fedce863
Vasko Zdravevski <vzdravevski@gmail.com>
2017-03-07 20:50:16 -0700

    problem 1.2 add benchmarks

--- chapter1/two/two_test.go
+++ chapter1/two/two_test.go
@@ -13,6 +13,16 @@ var testcases = []struct {
 	{[]string{"off", "oof"}, false},
 }
 
+func generateUniqueString(length int) string {
+	var c rune = 0
+	var result []rune
+	for i := 0; i < length; i++ {
+		result = append(result, c)
+		c++
+	}
+	return string(result)
+}
+
 func TestIsPermutation(t *testing.T) {
 	for _, c := range testcases {
 		if IsPermutation(c.in[0], c.in[1]) != c.out {
@@ -26,5 +36,37 @@ func TestIsPermutationDS(t *testing.T) {
 		if IsPermutationDS(c.in[0], c.in[1]) != c.out {
 			t.Errorf("IsPermutationDS given %s and %s but got %t\n", c.in[0], c.in[1], c.out)
 		}
+	}
+}
+
+func BenchmarkIsPermutation(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, c := range testcases {
+			IsPermutation(c.in[0], c.in[1])
+		}
+	}
+}
+
+func BenchmarkIsPermutationDS(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, c := range testcases {
+			IsPermutationDS(c.in[0], c.in[1])
+		}
+	}
+}
+
+func BenchmarkIsPermutation1kString(b *testing.B) {
+	s := generateUniqueString(1000)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		IsPermutation(s, s)
+	}
+}
+
+func BenchmarkIsPermutationDS1kString(b *testing.B) {
+	s := generateUniqueString(1000)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		IsPermutationDS(s, s)
 	}
 }
