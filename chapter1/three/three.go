1b1034373000d377260a07465e60f3d9b32d9e5b
Vasko Zdravevski <vzdravevski@gmail.com>
2017-03-08 22:05:04 -0700

    problem 1.3 URLify

---
+++ chapter1/three/three.go
@@ -0,0 +1,25 @@
+package three
+
+import "strings"
+
+func InplaceURLify(str string) string {
+	input := []rune(str)
+	endIndex := len(input) - 1
+	for i := range input {
+		if input[len(input)-1-i] != ' ' {
+			input[endIndex] = input[len(input)-1-i]
+			endIndex--
+		} else if endIndex != len(input)-1 {
+			input[endIndex] = '0'
+			input[endIndex-1] = '2'
+			input[endIndex-2] = '%'
+			endIndex -= 3
+		}
+	}
+	return string(input)
+}
+
+func SimpleURLify(str string) string {
+	str = strings.TrimSpace(str)
+	return strings.Replace(str, " ", "%20", -1)
+}
---
+++ chapter1/three/three_test.go
@@ -0,0 +1,47 @@
+package three
+
+import (
+	"reflect"
+	"testing"
+)
+
+var testcases = []struct {
+	in  string
+	out string
+}{
+	{"a", "a"},
+	{"a a  ", "a%20a"},
+	{"Mr John Smith    ", "Mr%20John%20Smith"},
+}
+
+func TestInplaceURLify(t *testing.T) {
+	for _, c := range testcases {
+		if out := InplaceURLify(c.in); !reflect.DeepEqual(out, c.out) {
+			t.Errorf("InplaceURLify: given %v expected %v but got %v\n", c.in, c.out, out)
+		}
+	}
+}
+
+func TestSimpleURLify(t *testing.T) {
+	for _, c := range testcases {
+		if out := SimpleURLify(c.in); !reflect.DeepEqual(out, c.out) {
+			t.Errorf("SimpleURLify: given %v expected %v but got %v\n", c.in, c.out, out)
+		}
+	}
+}
+
+func BenchmarkInplaceURLify(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, c := range testcases {
+			InplaceURLify(c.in)
+		}
+	}
+}
+
+func BenchmarkSimpleURLify(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, c := range testcases {
+			SimpleURLify(c.in)
+		}
+	}
+}
