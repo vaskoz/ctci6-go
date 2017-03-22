e241ee0851ee35eaa0093baf808c8531d4eb39ff
Vasko Zdravevski <vzdravevski@gmail.com>
2017-03-21 21:33:23 -0600

    problem 4.1 depth first search between nodes for directed graph

---
+++ chapter4/one/one.go
@@ -0,0 +1,23 @@
+package one
+
+type DiGraph map[int]map[int]struct{}
+
+func RouteExists(graph DiGraph, start, end int) bool {
+	var stack []int
+	discovered := make(map[int]struct{})
+	stack = append(stack, start)
+	for len(stack) > 0 {
+		var v int
+		v, stack = stack[len(stack)-1], stack[:len(stack)-1]
+		if _, found := discovered[v]; !found {
+			discovered[v] = struct{}{}
+			for k, _ := range graph[v] {
+				if k == end {
+					return true
+				}
+				stack = append(stack, k)
+			}
+		}
+	}
+	return false
+}
---
+++ chapter4/one/one_test.go
@@ -0,0 +1,51 @@
+package one
+
+import "testing"
+
+var testcases = []struct {
+	data       DiGraph
+	start, end int
+	out        bool
+}{
+	{
+		data: DiGraph{
+			1: map[int]struct{}{
+				3: struct{}{},
+				4: struct{}{},
+			},
+			2: map[int]struct{}{
+				3: struct{}{},
+			},
+			3: map[int]struct{}{
+				4: struct{}{},
+			},
+		},
+		start: 1,
+		end:   4,
+		out:   true,
+	},
+	{
+		data: DiGraph{
+			1: map[int]struct{}{
+				2: struct{}{},
+			},
+			2: map[int]struct{}{
+				4: struct{}{},
+			},
+			3: map[int]struct{}{
+				1: struct{}{},
+			},
+		},
+		start: 1,
+		end:   3,
+		out:   false,
+	},
+}
+
+func TestRouteExists(t *testing.T) {
+	for _, c := range testcases {
+		if out := RouteExists(c.data, c.start, c.end); out != c.out {
+			t.Errorf("start %v end %v expected %t got %t", c.start, c.end, c.out, out)
+		}
+	}
+}
