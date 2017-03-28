da2191f8ed9dcb42e44bcc8f7b4353dc2b9b6cda
Vasko Zdravevski <vzdravevski@gmail.com>
2017-03-27 21:10:25 -0600

    problem 10.1 merge two sorted arrays

---
+++ chapter10/one/one.go
@@ -0,0 +1,25 @@
+package one
+
+func Merge(a, b []int) []int {
+	result := make([]int, 0, len(a)+len(b))
+	i, j := 0, 0
+	for {
+		if i < len(a) && j < len(b) {
+			if a[i] < b[j] {
+				result = append(result, a[i])
+				i++
+			} else {
+				result = append(result, b[j])
+				j++
+			}
+		} else if i < len(a) {
+			result = append(result, a[i])
+			i++
+		} else if j < len(b) {
+			result = append(result, b[j])
+			j++
+		} else {
+			return result
+		}
+	}
+}
---
+++ chapter10/one/one_test.go
@@ -0,0 +1,23 @@
+package one
+
+import (
+	"reflect"
+	"testing"
+)
+
+var testcases = []struct {
+	a   []int
+	b   []int
+	out []int
+}{
+	{a: []int{1, 3, 5}, b: []int{2, 4, 6}, out: []int{1, 2, 3, 4, 5, 6}},
+	{a: []int{1, 3, 5}, b: []int{2}, out: []int{1, 2, 3, 5}},
+}
+
+func TestMerge(t *testing.T) {
+	for _, c := range testcases {
+		if out := Merge(c.a, c.b); !reflect.DeepEqual(c.out, out) {
+			t.Errorf("Expected %v but got %v", c.out, out)
+		}
+	}
+}
