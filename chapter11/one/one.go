4019777949b709ab80aa7336cf59088de7300250
Vasko Zdravevski <vzdravevski@gmail.com>
2017-03-29 19:20:10 -0600

    problem 11.1 more idiomatic max uint

--- chapter11/one/one.go
+++ chapter11/one/one.go
@@ -6,8 +6,7 @@ import (
 
 func Mistake() error {
 	var i uint
-	var maxUint uint = 0
-	maxUint-- // get max uint for this architecture
+	var maxUint uint = ^uint(0) // get max uint for this architecture
 	for i = 1; i >= 0; i-- {
 		if i == maxUint {
 			return errors.New("equal to MaxUint")
