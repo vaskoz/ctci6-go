99d262408d9f6836338b14d53965468e050ccec0
Vasko Zdravevski <vzdravevski@gmail.com>
2017-04-11 19:53:07 -0600

    problem 16.9 - add more testcases

--- chapter16/nine/nine.go
+++ chapter16/nine/nine.go
@@ -9,6 +9,17 @@ func Subtract(a, b int) int {
 }
 
 func Multiply(a, b int) int {
+	var neg bool = false
+	if a < 0 {
+		a = -a
+		neg = true
+	}
+	if b < 0 {
+		b = -b
+		if neg {
+			neg = false
+		}
+	}
 	var small, large int
 	if a < b {
 		small = a
@@ -21,16 +32,39 @@ func Multiply(a, b int) int {
 	for i := 1; i <= small; i++ {
 		result = Add(result, large)
 	}
-	return result
+	if neg {
+		return -result
+	} else {
+		return result
+	}
 }
 
 func Divide(a, b int) int {
+	var neg bool = false
+	if a < 0 {
+		a = -a
+		neg = true
+	}
+	if b < 0 {
+		b = -b
+		if neg {
+			neg = false
+		}
+	}
 	result := 0
 	for ; Multiply(result, b) < a; result++ {
 	}
 	if Multiply(result, b) > a {
-		return result - 1
+		if neg {
+			return -result + 1
+		} else {
+			return result - 1
+		}
 	} else {
-		return result
+		if neg {
+			return -result
+		} else {
+			return result
+		}
 	}
 }
--- chapter16/nine/nine_test.go
+++ chapter16/nine/nine_test.go
@@ -9,6 +9,10 @@ var testcases = []struct {
 	{a: 100, b: 100, add: 200, subtract: 0, multiply: 10000, divide: 1},
 	{a: 1, b: 100000, add: 100001, subtract: -99999, multiply: 100000, divide: 0},
 	{a: 1000000, b: 2, add: 1000002, subtract: 999998, multiply: 2000000, divide: 500000},
+	{a: -1, b: 2, add: 1, subtract: -3, multiply: -2, divide: 0},
+	{a: -1, b: -2, add: -3, subtract: 1, multiply: 2, divide: 0},
+	{a: -4, b: -2, add: -6, subtract: -2, multiply: 8, divide: 2},
+	{a: -4, b: 2, add: -2, subtract: -6, multiply: -8, divide: -2},
 }
 
 func TestAdd(t *testing.T) {
