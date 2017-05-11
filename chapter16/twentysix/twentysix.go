192a792111db8f7b23a4d2519dbd4deed59aff7e
Vasko Zdravevski <vzdravevski@gmail.com>
2017-05-10 21:15:06 -0600

    problem 16.26 - simple infix calculator

---
+++ chapter16/twentysix/twentysix.go
@@ -0,0 +1,66 @@
+package twentysix
+
+import (
+	"log"
+	"strconv"
+)
+
+func Calculate(input string) float64 {
+	operands := []float64{}
+	operators := []string{}
+	index := 0
+	for i, r := range input {
+		if r == '*' || r == '+' || r == '-' || r == '/' {
+			operands = append(operands, ParseFloat(input[index:i]))
+			operators = append(operators, string(r))
+			index = i + 1
+		}
+	}
+	operands = append(operands, ParseFloat(input[index:]))
+	operandLen, operatorLen := len(operands), len(operators)
+	result := 0.0
+	for i, o := range operators {
+		if o == "*" {
+			if result == 0.0 {
+				result = operands[i] * operands[i+1]
+			} else {
+				result *= operands[i+1]
+			}
+		} else if o == "/" {
+			if result == 0.0 {
+				result = operands[i] / operands[i+1]
+			} else {
+				result /= operands[i+1]
+			}
+		} else {
+			if result != 0.0 {
+				operands = append(operands, result)
+				result = 0.0
+			} else {
+				operands = append(operands, operands[i])
+			}
+			operators = append(operators, o)
+		}
+	}
+	operands = append(operands, operands[operandLen-1])
+	operands = operands[operandLen:]
+	operators = operators[operatorLen:]
+	result = operands[0]
+	for i, o := range operators {
+		if o == "+" {
+			result += operands[i+1]
+		} else if o == "-" {
+			result -= operands[i+1]
+		}
+	}
+	return result
+}
+
+func ParseFloat(val string) float64 {
+	var num float64
+	var err error
+	if num, err = strconv.ParseFloat(val, 64); err != nil {
+		log.Panicf("Got an invalid operand '%v'\n", err)
+	}
+	return num
+}
---
+++ chapter16/twentysix/twentysix_test.go
@@ -0,0 +1,39 @@
+package twentysix
+
+import "testing"
+
+var testcases = []struct {
+	input  string
+	output float64
+}{
+	{"2*3+5/6*3+15", 23.5},
+	{"231.2*3.1+5/6*3+15", 734.22},
+	{"1+2+3*6+7", 28.0},
+	{"256/2/2/8", 8.0},
+	{"256-256", 0.0},
+}
+
+func TestCalculate(t *testing.T) {
+	for _, c := range testcases {
+		if output := Calculate(c.input); output != c.output {
+			t.Errorf("Expected %v, got %v", c.output, output)
+		}
+	}
+}
+
+func TestBadCalculateInput(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected a panic on bad input")
+		}
+	}()
+	Calculate("256a-1xf")
+}
+
+func BenchmarkCalculate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, c := range testcases {
+			Calculate(c.input)
+		}
+	}
+}
