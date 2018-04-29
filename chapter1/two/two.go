71baea423d92e859a18d140dc9d6d2791a7ad0e4
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-04-28 21:47:20 -0600

    use reflect.DeepEqual to check slice equality

--- chapter1/two/two.go
+++ chapter1/two/two.go
@@ -1,6 +1,9 @@
 package two
 
-import "sort"
+import (
+	"reflect"
+	"sort"
+)
 
 func IsPermutation(s1, s2 string) bool {
 	if len(s1) != len(s2) {
@@ -9,12 +12,7 @@ func IsPermutation(s1, s2 string) bool {
 	r1, r2 := []rune(s1), []rune(s2)
 	sort.Slice(r1, func(i, j int) bool { return r1[i] < r1[j] })
 	sort.Slice(r2, func(i, j int) bool { return r2[i] < r2[j] })
-	for i := range r1 {
-		if r1[i] != r2[i] {
-			return false
-		}
-	}
-	return true
+	return reflect.DeepEqual(r1, r2)
 }
 
 func IsPermutationDS(s1, s2 string) bool {
