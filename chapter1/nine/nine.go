51fd31b40a3ff6c345f1cd5922f0d21f71fc3903
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-04-28 22:40:14 -0600

    don't export an unnecessary function

--- chapter1/nine/nine.go
+++ chapter1/nine/nine.go
@@ -6,9 +6,5 @@ import "strings"
 // Concatenates a string to itself to check for rotation which doubles space.
 func StringRotation(s1, s2 string) bool {
 	t := s2 + s2
-	return IsSubstring(t, s1)
-}
-
-func IsSubstring(s, substr string) bool {
-	return strings.Contains(s, substr)
+	return strings.Contains(t, s1)
 }
