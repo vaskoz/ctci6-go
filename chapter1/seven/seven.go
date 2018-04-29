b691f29f3cd19739e2d361178d7b27fd8add63ee
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-04-28 22:30:51 -0600

    godoc chapter1/seven.go

--- chapter1/seven/seven.go
+++ chapter1/seven/seven.go
@@ -1,5 +1,7 @@
 package seven
 
+// MatrixRotation90Inplace rotates a matrix of ints 90 degrees in place.
+// Runtime is O(elements_in_matrix) with constant additional space.
 func MatrixRotation90Inplace(matrix [][]int) [][]int {
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix)/2; j++ {
