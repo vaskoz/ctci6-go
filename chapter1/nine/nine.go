b835500316492ef536f6639a02a512571ef73bbc
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-04-28 22:38:56 -0600

    godoc chapter1/nine.go

--- chapter1/nine/nine.go
+++ chapter1/nine/nine.go
@@ -2,6 +2,8 @@ package nine
 
 import "strings"
 
+// StringRotation checks if strings are rotations of each other.
+// Concatenates a string to itself to check for rotation which doubles space.
 func StringRotation(s1, s2 string) bool {
 	t := s2 + s2
 	return IsSubstring(t, s1)
