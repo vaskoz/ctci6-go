628b73580790f6d5b278a98f58c675fdddd7a5fe
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-04-28 21:40:02 -0600

    godoc chapter1/one.go

--- chapter1/one/one.go
+++ chapter1/one/one.go
@@ -2,6 +2,8 @@ package one
 
 import "sort"
 
+// IsUnique checks if each rune in the string is unique using a map.
+// Runs in time O(N) and space O(N)
 func IsUnique(str string) bool {
 	m := map[rune]int{}
 	for _, r := range str {
@@ -13,6 +15,8 @@ func IsUnique(str string) bool {
 	return true
 }
 
+// IsUniqueNoDS checks if each rune in the string is unique without a data structure.
+// Runs in time O(N^2)
 func IsUniqueNoDS(str string) bool {
 	runes := []rune(str)
 	for i := range runes {
@@ -25,6 +29,8 @@ func IsUniqueNoDS(str string) bool {
 	return true
 }
 
+// IsUniqueSort checks for rune uniqueness by first sorting the string's runes.
+// Runs in time O(N log N) due to sorting before the linear scan.
 func IsUniqueSort(str string) bool {
 	runes := []rune(str)
 	less := func(i, j int) bool { return runes[i] < runes[j] }
