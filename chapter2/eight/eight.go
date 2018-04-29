18c62508a568b54dc58e1a19195f1a374f6af1b7
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-04-29 00:11:22 -0600

    godoc chapter2/eight.go

--- chapter2/eight/eight.go
+++ chapter2/eight/eight.go
@@ -1,10 +1,14 @@
 package eight
 
+// Elem represents a node in a singly linked list.
 type Elem struct {
 	Value interface{}
 	next  *Elem
 }
 
+// DetectLoop returns a pointer to the element that causes the loop in the singly linked list.
+// Runtime is O(N) as it need to traverse the entire list.
+// Space is also O(N) to store seen nodes.
 func DetectLoop(head *Elem) *Elem {
 	seen := map[*Elem]struct{}{}
 	for head != nil {
