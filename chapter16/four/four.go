75ca5a8871d168f202216dd82a21c65d27e68116
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-05-01 19:19:57 -0600

    godoc chapter16/four.go

--- chapter16/four/four.go
+++ chapter16/four/four.go
@@ -1,15 +1,18 @@
 package four
 
+// Constants to represent an empty cell and also 'X' and 'O'.
 const (
 	E = iota // E is Empty
 	X
 	O
 )
 
+// FullRow answers true if an entire row is controlled by either 'X' or 'O'.
 func FullRow(rows int, m map[int]int) bool {
 	return len(m) == 1 && (m[X] == rows || m[O] == rows)
 }
 
+// HasWon checks if any player has won this game of tic-tac-toe.
 func HasWon(board [][]int) bool {
 	rows := len(board)
 	cols := len(board[0])
