f165dad29fd2672a93bebcd43ca7f3731dee8b77
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-05-03 20:04:08 -0600

    godoc chapter16/one.go

--- chapter16/one/one.go
+++ chapter16/one/one.go
@@ -1,14 +1,18 @@
 package one
 
+// NumberSwap swaps the values pointed to by x and y using XOR.
 func NumberSwap(x, y *int) {
 	*x ^= *y
 	*y ^= *x
 }
 
+// NumberSwapGo swaps the values pointed to by x and y using parallel assignment.
+// https://en.wikipedia.org/wiki/Assignment_(computer_science)#Parallel_assignment
 func NumberSwapGo(x, y *int) {
 	*x, *y = *y, *x
 }
 
+// NumberSwapTemp swaps the values pointed to by x and y using a temporary variable.
 func NumberSwapTemp(x, y *int) {
 	temp := *x
 	*x = *y
