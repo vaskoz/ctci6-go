194a268ac3bbf4a4e12549fde320b39043378b05
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-05-03 20:18:58 -0600

    godoc chapter16/eleven.go

--- chapter16/eleven/eleven.go
+++ chapter16/eleven/eleven.go
@@ -1,5 +1,7 @@
 package eleven
 
+// DivingBoards returns all possible lengths of a diving board that can be constructed.
+// You must use exactly k boards for each answer of either shorter or longer board lengths.
 func DivingBoards(shorter, longer, k int) []int {
 	if shorter == longer {
 		return []int{shorter * k}
